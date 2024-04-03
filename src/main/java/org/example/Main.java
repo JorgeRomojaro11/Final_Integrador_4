3b3b23085302f12a2e6c2a0a9ea74b69cefa889b
JorgeRomojaro11 <jromodel@myuax.com>
2024-04-03 20:33:51 +0200

    main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import javax.swing.*;
+import Editor.TextEditor;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SwingUtilities.invokeLater(() -> {
+            TextEditor gui = new TextEditor();
+            gui.setVisible(true);
+        });
     }
 }
