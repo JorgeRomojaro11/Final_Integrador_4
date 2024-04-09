d5c8134c8f79582c603cdd2bc2912d295a83bbfd
JorgeRomojaro11 <jromodel@myuax.com>
2024-04-09 20:51:42 +0200

    main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 import javax.swing.*;
 import java.awt.*;
 import Editor.Herramientas;
+import Editor.Inicio;
 import Email.EmailValidator;
 
 import java.awt.event.MouseEvent;
@@ -11,10 +12,16 @@ import java.awt.event.MouseMotionAdapter;
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
+            // Crea y muestra la interfaz de inicio
+            Inicio inicio = new Inicio();
+            inicio.setVisible(true);
+
+            // Crea y muestra la interfaz principal
             Herramientas gui = new Herramientas();
             gui.setVisible(true);
+            gui.setLocationRelativeTo(null); // Centra la ventana en la pantalla
 
-            // Add a mouse motion listener to the text area to update the mouse position label
+            // Añade un listener de movimiento del ratón al área de texto para actualizar la etiqueta de posición del ratón
             gui.getTextArea().addMouseMotionListener(new MouseMotionAdapter() {
                 @Override
                 public void mouseMoved(MouseEvent e) {
@@ -22,7 +29,7 @@ public class Main {
                 }
             });
 
-            // Add the email validator to the GUI
+            // Añade el validador de correo electrónico a la GUI
             EmailValidator emailValidator = new EmailValidator();
             gui.getContentPane().add(emailValidator, BorderLayout.NORTH);
         });
